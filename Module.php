5c8a2684a4acbe7a08cf726606d31b4fd91f6f83
Daniel Gimenes <daniel@danielgimenes.com.br>
2013-12-06 17:38:16 -0200

    Let Zend/Form/Factory inject hydrator and object inside fieldsets

--- Module.php
+++ Module.php
@@ -9,42 +9,8 @@
 
 namespace FhcsFormTest;
 
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-use Zend\ModuleManager\Feature\FormElementProviderInterface;
-use FhcsFormTest\Form\ProductFieldset;
-use FhcsFormTest\Form\OrderFieldset;
-use FhcsFormTest\Form\FreightFieldset;
-
-class Module implements FormElementProviderInterface
+class Module
 {
-    
-    public function getFormElementConfig()
-    {
-        return array(
-            'factories' => array(
-                'FhcsFormTest\Form\ProductFieldset' => function($sm) {
-                    $entityManager = $sm->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-                    $fieldset = new ProductFieldset();
-                    $fieldset->setEntityManager($entityManager);
-                    return $fieldset;
-                },
-                'FhcsFormTest\Form\OrderFieldset' => function($sm) {
-                    $entityManager = $sm->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-                    $fieldset = new OrderFieldset();
-                    $fieldset->setEntityManager($entityManager);
-                    return $fieldset;
-                },
-                'FhcsFormTest\Form\FreightFieldset' => function($sm) {
-                    $entityManager = $sm->getServiceLocator()->get('Doctrine\ORM\EntityManager');
-                    $fieldset = new FreightFieldset();
-                    $fieldset->setEntityManager($entityManager);
-                    return $fieldset;
-                },
-            )
-        );
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
--- src/FhcsFormTest/Form/OrderFieldset.php
+++ src/FhcsFormTest/Form/OrderFieldset.php
@@ -3,32 +3,20 @@
 namespace FhcsFormTest\Form;
 
 use Zend\Form\Fieldset;
-use FhcsFormTest\Entity\Order;
-use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
 
 class OrderFieldset extends Fieldset {
 
-    protected $entityManager;
-
-    public function getEntityManager() {
-        return $this->entityManager;
-    }
-
-    public function setEntityManager($entityManager) {
-        $this->entityManager = $entityManager;
-    }
-
     public function init(){
-        $this->setHydrator(new DoctrineHydrator($this->getEntityManager(), 'FhcsFormTest\Entity\Order'))->setObject(new Order());
         $this->add(array(
             'type' => 'Zend\Form\Element\Collection',
             'name' => 'freights',
             'options' => array(
                 'target_element' => array(
-                    'type' => 'FhcsFormTest\Form\FreightFieldset'
+                    'type'     => 'FhcsFormTest\Form\FreightFieldset',
+                    'hydrator' => 'DoctrineModule\Stdlib\Hydrator\DoctrineObject',
+                    'object'   => 'FhcsFormTest\Entity\Freight',
                 )
             )
         ));
-        $this->get('freights')->setHydrator(new DoctrineHydrator($this->getEntityManager(), 'FhcsFormTest\Entity\Order'));
     }
 }
